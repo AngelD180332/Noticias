cc107df19ed1aeda2f2c2e54711b1df48c0699a4
Angel David <angeldivad.100089@gmail.com>
2026-02-27 16:36:41 -0600

    http

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,26 +1,46 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, SegmentChangeEventDetail } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
+import { NgFor } from '@angular/common';
 import { IonSegment, IonSegmentButton } from '@ionic/angular/standalone';
+import { ComponentsModule } from 'src/app/components/components-module';
+import { OnInit } from '@angular/core';
+import { NewsService } from '../../services/news';
+import { Article } from 'src/app/interfaces';
+import { ArticlesComponent } from 'src/app/components/articles/articles.component'; 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel,
-            ExploreContainerComponent,
-            IonSegment, IonSegmentButton
+            ExploreContainerComponent, NgFor,
+            IonHeader, IonToolbar, IonTitle, IonContent, IonLabel,
+            IonSegment, IonSegmentButton, ComponentsModule,
+            ArticlesComponent
           ],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   public categories: string[] = ['general', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'];
   public selectedCategory: string = this.categories[0];
+  public articles: Article[] = [];
 
-  constructor() {}
+  constructor( private newsService: NewsService ) {}
 
-  segmentChanged( category: any ) {
-    console.log(category);
-  }
+  ngOnInit(){
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory).subscribe(articles => {
+        this.articles = [ ...this.articles, ...articles];
+      }); 
+    };
+  
+
+  segmentChanged( event: any ) {
+    this.selectedCategory = event.detail.value;
+    this.newsService.getTopHeadlinesByCategory(this.selectedCategory).subscribe(articles => {
+        this.articles = [ ...articles];
+      });
+    };
+  
 
 }
--- src/app/services/news.ts
+++ src/app/services/news.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { NewResponse, Article } from '../interfaces';
-import { map } from 'rxjs/operators';
+import { count, map } from 'rxjs/operators';
 
 const apiKey = environment.apiKey;
 
@@ -16,6 +16,17 @@ export class NewsService {
 
   private apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`;
 
+  private executeQuery<T>( endpoint: string ) {
+    console.log('Petición HTTP realizada');
+    return this.http.get<T>(`${this.apiUrl}${endpoint}`, {
+      params: {
+        apiKey: apiKey,
+        country: 'us',
+      }
+    })
+  }
+
+
   getTopHeadlines(): Observable<Article[]> {
     return this.http.get<NewResponse>(this.apiUrl, {
       params: { apiKey }
@@ -23,4 +34,14 @@ export class NewsService {
       map(({articles}) => articles)
     );
   }
+
+  getTopHeadlinesByCategory(category: string): Observable<Article[]> {
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}`;
+    return this.http.get<NewResponse>(apiUrl, {
+      params: { apiKey }
+    }).pipe(
+      map(({articles}) => articles)
+    );
+  }
+
 }
